825d1823ab530cf569a13235b947ff9046cb7f18
Dururas <dramirezmoreno2014@gmail.com>
2025-02-08 20:19:15 -0500

    Update denis.js

--- denis.js
+++ denis.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementById('Button');
-
-let textField = document.getElementById('nameInput');
-
-    if (button) {
-        button.addEventListener('click', function() {				 textField.style.display = 'none'
-            this.style.display = 'none'; // 'this' refers to the clicked button
-        });
-    }
-});
 
